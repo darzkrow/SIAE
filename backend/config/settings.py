d26e2e9386f610e247daf0a1b09c495b0c0ff870
DarzKrow <gfrancojf@gmail.com>
2026-01-14 23:09:50 -0400

    refactor: Reestructurar proyecto moviendo todo el backend a una carpeta 'backend' - Mover aplicaciones, configuraciones y scripts de Django a la carpeta 'backend'. - Actualizar Dockerfile, docker-compose.yml y archivos de configuración para reflejar la nueva estructura. - Mover archivos .env y .env.example a la carpeta 'backend'.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,10 +2,10 @@
 import os
 from pathlib import Path
 import environ
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
+# Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 env = environ.Env(
     # set casting, default value
